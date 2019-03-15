a7104e07de6fe5ebe0a9d2f66eb28b908df35416
Francesco Fornari <forno96@gmail.com>
2019-03-15 19:27:41 +0100

    json requested only one time

--- loader.js
+++ loader.js
@@ -1,13 +1,14 @@
 function main() {
   $.getJSON("./database.json", function(data) {
-    db = data;
-    dbSize = db.info.length - 1;
+    index = data.info;
     start();
   });
 }
 
 function start() {
   progress = 0; total = 0; imgLenght = 0;
+  dbCap = index.length - 1;
+
   getRange();
   getImage();
 }
@@ -27,8 +28,8 @@ function getRange() {
   to = $("#to").val() - 1;
   from = $("#from").val() - 1;
   // the first check is for the first acces
-  to = (to >= 0 && to <= dbSize) ? to : 0
-  from = (from >= 0 && from <= dbSize) ? from : dbSize
+  to = (to >= 0 && to <= (index.length - 1)) ? to : 0
+  from = (from >= 0 && from <= (index.length - 1)) ? from : index.length - 1
 
   if (to > from) {
     to = to + from;
@@ -36,15 +37,13 @@ function getRange() {
     to = to - from;
   } // swap
 
-  db.info.splice(0,to);
-  db.info.splice(from+1,dbSize);
-
+  db = index.slice(to,from+1);
 }
 
 function getImage() {
   var elem;
-  if (db.info.length > 0) {
-    elem = db.info.splice(Math.floor(Math.random() * db.info.length),1)[0];
+  if (db.length > 0) {
+    elem = db.splice(Math.floor(Math.random() * db.length),1)[0];
     $("body").html(`
       <div class="container with-title container-fluid mt-3">
         <h2 class="title">${progress}/${total} foto indovinate</h2>
@@ -60,12 +59,12 @@ function getImage() {
           </div>
           <button type="button" class="btn" id="info-button" onclick="addInfo()">Mostra info</button>
         </div>
-        <button type="button" class="btn is-success" onclick="main()">Cambia Range</button>
+        <button type="button" class="btn is-success" onclick="start()">Cambia Range</button>
         <div class="field is-inline mt-1">
           <label for="to" class="m-1">Da</label>
-          <input class="input" style="max-width: 94px" id="to" type="number" min="1" max="${dbSize}" value="${to+1}">
+          <input class="input" style="max-width: 94px" id="to" type="number" min="1" max="${dbCap}" value="${to+1}">
           <label for="from" style="max-width: 18px" class="m-1">a</label>
-          <input class="input" style="max-width: 94px" id="from" type="number" min="1" max="${dbSize}" value="${from+1}">
+          <input class="input" style="max-width: 94px" id="from" type="number" min="1" max="${dbCap}" value="${from+1}">
         </div>
       </div>`);
     $("#info-box").hide();
@@ -76,13 +75,13 @@ function getImage() {
         <h2> Hai visto tutte le immagini! </h2>
         <h3> Con ${progress} su ${total} immagini azzeccate! </h3>
 
-        <button type="button" class="btn" onclick="main()">Reinizia da capo</button>
-        <button type="button" class="btn is-success" onclick="main()">Cambia Range</button>
+        <button type="button" class="btn" onclick="start()">Reinizia da capo</button>
+        <button type="button" class="btn is-success" onclick="start()">Cambia Range</button>
         <div class="field is-inline">
           <label for="to" class="m-1">Da</label>
-          <input class="input" style="max-width: 94px" id="to" type="number" min="1" max="${db.info.length}" value="${to+1}">
+          <input class="input" style="max-width: 94px" id="to" type="number" min="1" max="${dbCap}" value="${to+1}">
           <label for="from" style="max-width: 18px" class="m-1">a</label>
-          <input class="input" style="max-width: 94px" id="from" type="number" min="1" max="${db.info.length}" value="${from+1}">
+          <input class="input" style="max-width: 94px" id="from" type="number" min="1" max="${dbCap}" value="${from+1}">
         </div>
       </div>`);
   }
