83bbaec11e30679a4c7216d24c79aecf0deee312
Francesco Fornari <forno96@gmail.com>
2019-03-18 18:05:53 +0100

    Less repeat

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Random-Image
 This project use [NES.css](https://github.com/BcRikko/NES.css) 2.1.0 and [jQuery](https://jquery.com) 3.3.1.
 
-This site was created for improve my memory about photography for the exam of "Storia della fotografia" by the professor Claudio Marra.
+This is a single page application was created for improve my memory about photography for the exam of "Storia della fotografia" by the professor Claudio Marra.
--- loader.js
+++ loader.js
@@ -6,11 +6,11 @@ function main() {
 }
 
 function start() {
-  progress = 0; total = 0; imgLenght = 0;
+  progress = 0; total = 0;
   dbCap = index.length - 1;
 
   getRange();
-  getImage();
+  showImage();
 }
 
 function addInfo() {
@@ -21,7 +21,7 @@ function addInfo() {
 function vote(get) {
   progress += get;
   total += 1;
-  getImage();
+  showImage();
 }
 
 function getRange() {
@@ -40,56 +40,49 @@ function getRange() {
   db = index.slice(to,from+1);
 }
 
-function getImage() {
+function showImage() {
   var elem;
   if (db.length > 0) {
     elem = db.splice(Math.floor(Math.random() * db.length),1)[0];
-    $("body").html(`
-      <div class="nes-container is-rounded with-title m-3">
-        <h2 class="title">${progress}/${total} foto indovinate</h2>
-        <div class="nes-container text-center">
-          <img class="img-fluid" style="max-height: 450px" src="./foto/${elem.url}" alt="Card image cap">
+    $("#container").html(`
+      <h2 class="title">${progress}/${total} foto indovinate</h2>
+      <div class="nes-container text-center">
+        <img class="img-fluid" style="max-height: 450px" src="./foto/${elem.url}" alt="Card image cap">
+      </div>
+      <div class="nes-container mt-2">
+        <div id="info-box">
+          <h5>${elem.author},</h5>
+          <p>${elem.name}</p>
+          <button type="button" class="nes-btn is-success" id="voteup" onclick="vote(1)">Indovinata</button>
+          <button type="button" class="nes-btn is-error" id="votedown" onclick="vote(0)">Non Indovinata</button>
         </div>
-        <div class="nes-container mt-2">
-          <div id="info-box">
-            <h5>${elem.author},</h5>
-            <p>${elem.name}</p>
-            <button type="button" class="nes-btn is-success" id="voteup" onclick="vote(1)">Indovinata</button>
-            <button type="button" class="nes-btn is-error" id="votedown" onclick="vote(0)">Non Indovinata</button>
-          </div>
-          <button type="button" class="nes-btn" id="info-button" onclick="addInfo()">Mostra info</button>
-        </div>
-        <div class="nes-field is-inline mt-3">
-          <button type="button" class="nes-btn is-primary" onclick="start()">Cambia Range</button>
-          <label for="to" class="m-1">Da</label>
-          <input class="nes-input" style="max-width: 94px" id="to" type="number" min="1" max="${dbCap}" value="${to+1}">
-          <label for="from" style="max-width: 18px" class="m-1">a</label>            <input class="nes-input" style="max-width: 94px" id="from" type="number" min="1" max="${dbCap}" value="${from+1}">
-        </div>
-        <br/>
-        <p class="text-right text-secondary">
-          Created by <a href="https://github.com/forno96">@forno96</a>
-        </p>
-      </div>`);
+        <button type="button" class="nes-btn" id="info-button" onclick="addInfo()">Mostra info</button>
+      </div>
+      <div id="range"></div>`);
     $("#info-box").hide();
   }
   else {
-    $("body").html(`
-      <div class="nes-container is-rounded text-center m-3">
+    $("#container").html(`
+      <div class="text-center">
         <h2> Hai visto tutte le immagini! </h2>
         <h3> Con ${progress} su ${total} immagini azzeccate! </h3>
 
         <button type="button" class="nes-btn" onclick="start()">Reinizia da capo</button>
-        <button type="button" class="nes-btn is-primary" onclick="start()">Cambia Range</button>
-        <div class="nes-field is-inline mt-3">
-          <label for="to" class="m-1">Da</label>
-          <input class="nes-input" style="max-width: 94px" id="to" type="number" min="1" max="${dbCap}" value="${to+1}">
-          <label for="from" style="max-width: 18px" class="m-1">a</label>
-          <input class="nes-input" style="max-width: 94px" id="from" type="number" min="1" max="${dbCap}" value="${from+1}">
-        </div>
-        <br/>
-        <p class="text-right text-secondary">
-          Created by <a href="https://github.com/forno96">@forno96</a>
-        </p>
+
+        <div id="range"></div>
       </div>`);
   }
+  $("#range").html(`
+    <div class="nes-field is-inline mt-3">
+      <button type="button" class="nes-btn is-primary" onclick="start()">Cambia Range</button>
+      <label for="to" class="m-1">Da</label>
+      <input class="nes-input" style="max-width: 94px" id="to" type="number" min="1" max="${dbCap}" value="${to+1}">
+      <label for="from" style="max-width: 18px" class="m-1">a</label>
+      <input class="nes-input" style="max-width: 94px" id="from" type="number" min="1" max="${dbCap}" value="${from+1}">
+    </div>
+
+    <br/>
+    <p class="text-right text-secondary">
+      Created by <a href="https://github.com/forno96">@forno96</a>
+    </p>`);
 }
